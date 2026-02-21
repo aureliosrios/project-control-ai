b7865652049d956f6024bc1620e671267d35dd16
project-control-ai <164905487+aureliosrios@users.noreply.github.com>
2026-02-21 10:55:12 -0500

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -49,7 +49,7 @@ const CURSOS_DISPONIBLES = [
     { id: 'c2', nombre: 'Control y Gerencia EVM', fecha_inicio: 'Feb 26, 2026 19:30:00' },
     { id: 'c3', nombre: 'Gestión de Proyectos con IA', fecha_inicio: 'Feb 22, 2026 10:00:00' },
     { id: 'c4', nombre: 'Automation Engineer', fecha_inicio: 'Feb 22, 2026 15:00:00' },
-    { id: 'c5', nombre: 'Primavera P6 Professional', fecha_inicio: 'Feb 20, 2026 19:00:00' },
+    { id: 'c5', nombre: 'Primavera P6 Professional', fecha_inicio: 'Feb 27, 2026 19:00:00' },
 
 ];
 
